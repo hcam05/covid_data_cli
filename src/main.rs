e43d95917a44d11ee4d672f6153a31bbf68f8509
hcam05 <hcam05@gmail.com>
2020-05-03 22:12:56 -0700

    separate reqwest logic, add hashmap lookup for states

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
+use std::io;
+
 #[macro_use]
 extern crate serde;
 extern crate reqwest;
+
 #[macro_use]
 extern crate prettytable;
 use prettytable::Table;
 
-use std::io;
-
-#[derive(Deserialize, Debug)]
+#[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct State {
     state: String,
@@ -20,15 +21,10 @@ struct State {
     tests_per_one_million: u32,
 }
 
-#[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
-    println!("Enter state");
-    let mut state_name = String::new();
+#[macro_use]
+extern crate maplit;
 
-    // TODO find better way to handle edge case where user doesnt enter anything
-    // TODO add more input validation
-    io::stdin().read_line(&mut state_name).unwrap();
-
+async fn get_data(state_name: String) -> Result<Vec<State>, reqwest::Error> {
     let url = format!("https://corona.lmao.ninja/v2/states?sort=cases");
 
     let response = reqwest::get(&url).await?;
@@ -36,18 +32,109 @@ async fn main() -> Result<(), reqwest::Error> {
 
     let state: Vec<State> = states
         .into_iter()
-        .filter(|state| state.state == state_name.trim())
+        .filter(|state| state.state == state_name)
         .collect();
 
-    // TODO is there a way to iterate over a struct?
+    Ok(state)
+}
+
+fn format_state_name(name: &mut String) -> String {
+    let _state_lookup = hashmap! {
+        "alabama" => "Alabama",
+        "alaska" => "Alaska",
+        "arizona" => "Arizona",
+        "arkansas" => "Arkansas",
+        "california" => "California",
+        "colorado" => "Colorado",
+        "connecticut" => "Connecticut",
+        "delaware" => "Delaware",
+        "florida" => "Florida",
+        "georgia" => "Georgia",
+        "hawaii" => "Hawaii",
+        "idaho" => "Idaho",
+        "illinois" => "Illinois",
+        "indiana" => "Indiana",
+        "iowa" => "Iowa",
+        "kansas" => "Kansas",
+        "kentucky" => "Kentucky",
+        "louisiana" => "Louisiana",
+        "maine" => "Maine",
+        "maryland" => "Maryland",
+        "massachusetts" => "Massachusetts",
+        "michigan" => "Michigan",
+        "minnesota" => "Minnesota",
+        "mississippi" => "Mississippi",
+        "missouri" => "Missouri",
+        "montana" => "Montana",
+        "nebraska" => "Nebraska",
+        "nevada" => "Nevada",
+        "new_hampshire" => "New Hampshire",
+        "new_jersey" => "New Jersey",
+        "new_mexico" => "New Mexico",
+        "new_york" => "New York",
+        "north_carolina" => "North Carolina",
+        "north_dakota" => "North Dakota",
+        "ohio" => "Ohio",
+        "oklahoma" => "Oklahoma",
+        "oregon" => "Oregon",
+        "pennsylvania" => "Pennsylvania",
+        "rhode_island" => "Rhode Island",
+        "south_carolina" => "South Carolina",
+        "south_dakota" => "South Dakota",
+        "tennessee" => "Tennessee",
+        "texas" => "Texas",
+        "utah" => "Utah",
+        "vermont" => "Vermont",
+        "virginia" => "Virginia",
+        "washington" => "Washington",
+        "west_virginia" => "West Virginia",
+        "wisconsin" => "Wisconsin",
+        "wyoming" => "Wyoming",
+    };
+
+    let clean_input = name
+        .trim()
+        .split_whitespace()
+        .collect::<Vec<&str>>()
+        .join("_")
+        .to_lowercase();
+
+    return _state_lookup.get::<str>(&clean_input).unwrap().to_string();
+}
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    println!("Enter state");
+    let mut input = String::new();
+
+    // TODO find better way to handle edge case where user doesnt enter anything
+    io::stdin().read_line(&mut input).unwrap();
+
+    let state_name = format_state_name(&mut input);
+    println!("{:?}", input);
+    println!("{:?}", state_name);
+
+    let state = get_data(state_name).await?;
+
     if state.len() > 0 {
+        let State {
+            state,
+            today_cases,
+            cases,
+            deaths,
+            active,
+            tests,
+            ..
+        } = &state[0];
+
+        // Create table
         let mut table = Table::new();
-        table.add_row(row![bFg->"Covid Cases", br -> state[0].state]);
-        table.add_row(row!["New Cases", r -> state[0].today_cases]);
-        table.add_row(row!["Total Cases", r -> state[0].cases]);
-        table.add_row(row!["Deaths", r -> state[0].deaths]);
-        table.add_row(row!["Active", r -> state[0].active]);
-        table.add_row(row!["Tests", r -> state[0].tests]);
+        table.add_row(row![bFg -> "Covid Cases", brFb -> state]);
+        table.add_row(row!["New Cases", r -> today_cases]);
+        table.add_row(row!["Total Cases", r -> cases]);
+        table.add_row(row!["Deaths", r -> deaths]);
+        table.add_row(row!["Active", r -> active]);
+        table.add_row(row!["Tests", r -> tests]);
         table.printstd();
     }
 
